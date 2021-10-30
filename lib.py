43f24eb9a48733eb22509e9ccd37e3d3fad6c13d
JPratama7 <josepratama080@gmail.com>
2021-10-30 23:33:41 +0800

    make code more readable TODO: FIX GET ENV ERROR

--- lib.py
+++ lib.py
@@ -1,4 +1,30 @@
+import os
+
+from mysql import connector
+from dotenv import load_dotenv
+
 def logfunc(type, e):
     type = str(type).upper()
     with open("logbot.txt", "a") as log:
         log.write(f"{type} ERROR : {e}\n" )
+
+def create_conn():
+    load_dotenv()
+    host = os.getenv("HOST")
+    user = "root"
+    password = os.getenv("PASSWORD")
+    database = os.getenv("DATABASE")
+    try:
+        db = connector.connect(host=host, user=user, password=password, database=database, autocommit=True)
+        return db
+    except Exception as e:
+        print(e)
+        logfunc("DB CONN NOT CREATED", e)
+
+def create_cursor():
+    db = create_conn()
+    try:
+        cursor = db.cursor()
+        return cursor
+    except Exception as e:
+        logfunc('CURSOR NOT CREATED', e)
--- main.py
+++ main.py
@@ -1,23 +1,15 @@
+import os
+
 from telebot import TeleBot
 from telebot import types
-from mysql import connector
+from dotenv import load_dotenv
 from lib import *
 # Global Variable
-bot = TeleBot("1757350571:AAFzg3sDUpYgngjRPjzK78OI3XYtlGvQD0U")
-host="localhost"
-user = "root"
-password = ""
-database = "bot"
+load_dotenv(".env")
+TOKEN = os.getenv("API")
+bot = TeleBot(TOKEN)
+
 user_dict = {}
-
-#Connect to database
-try:
-    db = connector.connect(host=host,user=user, password=password, database=database)
-    sql = db.cursor()
-    print("database connected")
-except Exception as e:
-    logfunc('database', e)
-
 
 class User:
     def __init__(self, teleg_id):
@@ -28,16 +20,20 @@ class User:
 @bot.message_handler(commands=['daftar'])
 def send_welcome(message):
     chat_id = int(message.chat.id)
-    query = f"SELECT tele_id FROM user WHERE tele_id={chat_id}"
-    sql.execute(query)
-    data = sql.fetchone()
-    if data != None:
-        bot.reply_to(message,"Anda sudah terdaftar")
-    else:
-        msg = bot.reply_to(message, """\
-    Silahkan jawab pertanyaan sesuai data diri.\nNama:
-    """)
-        bot.register_next_step_handler(msg, nama)
+    cursor = create_cursor()
+    try:
+        cursor.execute(f"SELECT tele_id FROM user WHERE tele_id={chat_id}")
+        data = cursor.fetchone()
+        if data != None:
+            bot.reply_to(message,"Anda sudah terdaftar")
+        else:
+            msg = bot.reply_to(message, """\
+        Silahkan jawab pertanyaan sesuai data diri.\nNama:
+        """)
+            bot.register_next_step_handler(msg, nama)
+        cursor.close()
+    except Exception as e:
+        logfunc("cursor error", e)
         
 
 def nama(message):
@@ -74,12 +70,16 @@ def commit_to_database(message):
         chat_id = message.chat.id
         keputusan = message.text
         user = user_dict[chat_id]
+        cursor = create_cursor()
         if (keputusan == u'ya'):
             insert = "INSERT INTO user (tele_id,nama,alamat) VALUES (%s,%s,%s)"
             val = (user.teleg_id,user.nama,user.alamat)
-            sql.execute(insert, val)
-            db.commit()
-            bot.send_message(chat_id,"ok data sudah terinput")
+            try:
+                cursor.execute(insert,val)
+                bot.send_message(chat_id,"ok data sudah terinput")
+            except Exception as e:
+                bot.send_message(chat_id,"terjadi error silahkan ulang kembali")
+                logfunc('commit database', e)
         else:
             bot.send_message(chat_id, "Silahkan tekan -> /daftar untuk melakukan pendaftaran ulang")
         # remove used object at user_dict
@@ -87,13 +87,15 @@ def commit_to_database(message):
     except Exception as e:
         logfunc('commit database',e)
         bot.reply_to(message, 'oooops terjadi error silahkan lapor ke admin terjadi error silahkan lapor ke admin')
+    cursor.close()
 
 #Handler Perintah list
 @bot.message_handler(commands=["list"])
 def product_list(message):
     try:
         chat_id = message.chat.id
-        sql.execute("SELECT * FROM barang")
+        cursor = create_cursor()
+        cursor.execute("SELECT * FROM barang")
         barang = sql.fetchall()
         for x in barang:
             id_barang,nama,harga,stock,gambar=x
@@ -102,23 +104,29 @@ def product_list(message):
     except Exception as e:
         logfunc('list', e)
         bot.send_message(chat_id=chat_id,text='oooops terjadi error silahkan lapor ke admin')
+    cursor.close()
 
 #handler  orderlist
 @bot.message_handler(commands=["orderlist"])
 def orderlist(message):
     tele_id = int(message.chat.id)
-    sql.execute("SELECT list_order.id_order,user.nama,barang.nama,list_order.total,list_order.jmlh,list_order.created_at FROM list_order INNER JOIN user ON list_order.tele_id = user.tele_id INNER JOIN barang ON list_order.barang = barang.id_barang WHERE list_order.tele_id = %s" % (tele_id))
-    data = sql.fetchall()
-    if len(data) != 0:
-        full_list = []
-        for datauser in data:
-            id_order, nama_user,nama_barang,total,jmlah,tanggal = datauser
-            pesan = f"ID ORDER = {id_order}\nNama User = {nama_user}\nNama Barang = {nama_barang}\nTotal Pembelian = {total}\nJumlah Pembelian = {jmlah}\nTanggal Pemesanan = {tanggal}\n"
-            full_list.append(pesan)
-        msg = '\n'.join(data)
-        bot.send_message(tele_id,msg)
-    else:
-        bot.send_message(tele_id,"belum melakukan pesanan")
+    cursor = create_cursor()
+    try:
+        cursor.execute("SELECT list_order.id_order,user.nama,barang.nama,list_order.total,list_order.jmlh,list_order.created_at FROM list_order INNER JOIN user ON list_order.tele_id = user.tele_id INNER JOIN barang ON list_order.barang = barang.id_barang WHERE list_order.tele_id = %s" % (tele_id))
+        data = cursor.fetchall()
+        if len(data) != 0:
+            full_list = []
+            for datauser in data:
+                id_order, nama_user,nama_barang,total,jmlah,tanggal = datauser
+                pesan = f"ID ORDER = {id_order}\nNama User = {nama_user}\nNama Barang = {nama_barang}\nTotal Pembelian = {total}\nJumlah Pembelian = {jmlah}\nTanggal Pemesanan = {tanggal}\n"
+                full_list.append(pesan)
+            msg = '\n'.join(data)
+            bot.send_message(tele_id,msg)
+        else:
+            bot.send_message(tele_id,"belum melakukan pesanan")
+    except Exception as e:
+        bot.send_message(chat_id, 'terjadi error silahkan lakukan perintah kembali')
+    cursor.close()
 
 print("bot berlari")
 bot.infinity_polling()
